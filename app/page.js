12fc356f47f5718ac30f609b7fd5e685fc0b0dfe
Kelly-E <kly.3939kingpeanut@gmail.com>
2026-06-28 10:42:28 +0900

    Remove AuthBanner component from Home

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 import dynamic from "next/dynamic";
-import AuthBanner from "../components/AuthBanner";
 
 // recharts等はブラウザ専用のため、サーバー側でレンダリングしない設定にする
 const MyBanker = dynamic(() => import("../components/MyBanker"), { ssr: false });
@@ -7,7 +6,6 @@ const MyBanker = dynamic(() => import("../components/MyBanker"), { ssr: false })
 export default function Home() {
   return (
     <main>
-      <AuthBanner />
       <MyBanker />
     </main>
   );
